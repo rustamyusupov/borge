0ce7297fcae684784f098bb6717ee737340d4a86
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-04-30 20:52:46 +0200

    feat(bin): ask and print user name

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!");
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!\n');
+
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
