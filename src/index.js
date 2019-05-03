af2300ff1772754d6da2b898daca4035d945bab3
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-05-03 13:58:03 +0200

    feat(index): add iscorrectanswer and getcorrectanswer funcs

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
 const isEven = value => value % 2 === 0;
 
-export { prompt, getRandomInt, isEven };
+const isCorrectAnswer = (number, answer) => {
+  const isEvenNumber = isEven(number);
+
+  return (
+    (answer !== 'yes' && answer !== 'no') || (isEvenNumber && answer === 'no') || (!isEvenNumber && answer === 'yes')
+  );
+};
+
+const getCorrectAnswer = number => (isEven(number) ? 'yes' : 'no');
+
+export {
+  prompt, getRandomInt, isCorrectAnswer, getCorrectAnswer,
+};
