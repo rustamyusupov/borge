e357b02dc158ea19d3449b61c6adec3d2b3ba353
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-05-03 21:19:00 +0200

    refactor(bin): remove unused brain-games

--- src/bin/brain-games.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import { prompt } from '..';
-
-console.log('Welcome to the Brain Games!\n');
-
-const userName = prompt('May I have your name?');
-
-console.log(`Hello, ${userName}`);
