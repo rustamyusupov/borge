9ede59b96fc0b3f43d87f0967a43a47fe52b4e51
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-05-03 21:48:19 +0200

    refactor(index): separate main games logic

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 import readlineSync from 'readline-sync';
 
+import { ROUNDS_COUNT } from './utils/constants';
+
 const prompt = question => readlineSync.question(`${question} `);
 
-const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+const greetingUser = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
 
-const isEven = value => value % 2 === 0;
+  const userName = prompt('May I have your name?');
+  console.log(`Hello, ${userName}\n`);
 
-const isCorrectAnswer = (number, answer) => {
-  const isEvenNumber = isEven(number);
-
-  return (
-    (answer === 'yes' || answer === 'no') && ((isEvenNumber && answer === 'yes') || (!isEvenNumber && answer === 'no'))
-  );
+  return userName;
 };
 
-const getCorrectAnswer = number => (isEven(number) ? 'yes' : 'no');
+const showResult = (result, userName) => console.log(`${result ? 'Congratulations' : "Let's try again"}, ${userName}!`);
 
-export {
-  prompt, getRandomInt, isCorrectAnswer, getCorrectAnswer,
+const flow = (qa = []) => {
+  let attempt = 0;
+
+  while (attempt < ROUNDS_COUNT) {
+    const question = qa[attempt][0];
+    console.log(`Question: ${question}`);
+
+    const correctAnswer = qa[attempt][1];
+    const answer = prompt('Your answer:');
+
+    if (answer !== correctAnswer) {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      break;
+    }
+
+    console.log('Correct!');
+    attempt += 1;
+  }
+
+  return attempt === 3;
 };
+
+const run = (qa) => {
+  const userName = greetingUser();
+  const result = flow(qa);
+
+  showResult(result, userName);
+};
+
+export default run;
