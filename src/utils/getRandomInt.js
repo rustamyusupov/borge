4683803494db6d1a5666ecad75fb79fdfbef0c18
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-05-05 14:41:33 +0200

    refactor(utils): add max arg to getrandomint

--- src/utils/getRandomInt.js
+++ src/utils/getRandomInt.js
@@ -1,3 +1,1 @@
-import { MAX_RANDOM_NUMBER } from './constants';
-
-export default () => Math.floor(Math.random() * Math.floor(MAX_RANDOM_NUMBER));
+export default max => Math.floor(Math.random() * Math.floor(max));
