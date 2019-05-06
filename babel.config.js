7a1b3966be4cbcc8925674cb1fbde3970c130b0e
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-05-06 17:20:08 +0200

    chore(babel): set target

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
 };
