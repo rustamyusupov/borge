495ca4a3fd81566e8a5fd3cb8171ae09310ae222
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-05-07 17:07:39 +0200

    refactor(games): optimize gcd func, rename res const

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,20 +4,14 @@ import gameEngine from '..';
 const gameRule = 'Find the greatest common divisor of given numbers.';
 const maxRandomNumber = 100;
 
-const gcd = (a, b) => {
-  if (!b) {
-    return a;
-  }
-
-  return gcd(b, a % b);
-};
+const gcd = (a, b) => (!b ? a : gcd(b, a % b));
 
 const quiz = () => {
   const a = getRandomInt(0, maxRandomNumber);
   const b = getRandomInt(0, maxRandomNumber);
-  const res = gcd(a, b);
+  const result = gcd(a, b);
   const question = `${a} ${b}`;
-  const answer = `${res}`;
+  const answer = `${result}`;
 
   return [question, answer];
 };
